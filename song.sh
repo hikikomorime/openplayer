463584d0a548685b7f0c08d83c7e4c1449442fda
hikikomorime <michal@skweres.net>
2020-05-16 07:32:42 +0200

    Update song.sh

--- song.sh
+++ song.sh
@@ -3,7 +3,7 @@ while true; do
 # define json from which you will take the current song title
 SONG_TITLE=$(curl -s 'https://YOUR_AZURACAST_URL/api/nowplaying_static/NAME_OF_YOUR_RADIO.json' | jshon -e "now_playing" -e "song" -e "title")
 
-echo $SONG_TITLE | tee piosenka.txt
+echo $SONG_TITLE | tee song.txt
 
   sleep 5;
 done
